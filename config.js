cbfb5087ae29703f281f44800b998d0102a94133
titata-del <titata0711@gmail.com>
2026-09-19 15:03:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.SCANMOOD_CONFIG = {
-  // Colle ici l’adresse donnée par Cloudflare après le déploiement gratuit.
-  // Exemple : "https://scanmood-api.ton-sous-domaine.workers.dev"
-  apiBase: ""
+  apiBase: "https://scanmood-api.titata0711.workers.dev"
 };
